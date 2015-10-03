35e074739f1c9434db8429e25ae56abea6b53420
John Doyle <john.c.doyle@gmail.com>
2015-10-03 12:57:29 -0400

    Yo built app
    Might as well use the Yo template rather than adding each test framework
    in individually

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xtracAssignmentApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the xtracAssignmentApp
+ */
+angular.module('xtracAssignmentApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xtracAssignmentApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the xtracAssignmentApp
+ */
+angular.module('xtracAssignmentApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
